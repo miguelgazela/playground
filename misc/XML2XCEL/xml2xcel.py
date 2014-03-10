26bd81b9d4923e48945a6ff1d5e884c8982d70e6
miguelgazela <miguel.gazela@gmail.com>
2014-03-10 13:32:41 +0000

    xml2xcel can be called with argv

--- misc/XML2XCEL/xml2xcel.py
+++ misc/XML2XCEL/xml2xcel.py
@@ -2,31 +2,59 @@
 
 import xml.etree.ElementTree as ET
 from xlwt import *
+from sys import argv
 
-def main():
+
+def get_xml_tree(filename):
+    """
+    Tries to parse a xml file and return a tree built from
+    its content.
+    """
+
     try:
-        tree = ET.parse('strings.xml')
+        return ET.parse(filename)
     except IOError:
-        print "The strings.xml file was not found."
-        return
+        print "The file {} was not found.".format(filename)
+        return None
     except ET.ParseError:
         print "Not well-formed XML file."
-        return
+        return None
 
-    root = tree.getroot()
+
+def save_strings_2_excel(strings, filename):
+    """
+    Saves a list of strings to an excel file
+    """
+
     workbook = Workbook()
     sheet = workbook.add_sheet('Android Strings')
     sheet.write(0, 0, 'String Name')
     sheet.write(0, 1, 'String Value')
 
-    # write xml content to the excel file
     counter = 1
-    for child in root.findall('string'):
-        sheet.write(counter, 0, child.get('name'))
-        sheet.write(counter, 1, child.text)
+    for string in strings:
+        sheet.write(counter, 0, string.get('name'))
+        sheet.write(counter, 1, string.text)
         counter += 1
 
-    workbook.save('strings.xls')
+    workbook.save(filename)
+
+
+def main():
+
+    if len(argv) == 3:
+        script, source, dest = argv
+    else:
+        print "Usage: xml2xcel.py SOURCE DEST"
+        return
+
+    tree = ET.parse(source)
+    if not tree:
+        return
+
+    root = tree.getroot()
+    save_strings_2_excel(root.findall('string'), dest)
+
 
 if __name__ == "__main__":
     main()
